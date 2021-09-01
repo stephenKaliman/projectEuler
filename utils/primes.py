78c1f9ae477507ce8b650cfbf4d5cc87b023ee72
stephenKaliman <64545785+stephenKaliman@users.noreply.github.com>
2021-09-01 11:24:22 -0700

    fixes + new version of function

--- utils/primes.py
+++ utils/primes.py
@@ -1,20 +1,33 @@
 import math as m
 
 def find_primes(upper):
-    if(upper<2):
-        return []
-    elif(upper==2):
-        return [2]
-    primes = [2]
+    primes = []
     for i in range(2,upper):
         inner_upper = m.sqrt(i)
         prime = True
         for p in primes:
-            if (p>=inner_upper):
+            if (p>inner_upper):
                 break
             if (i%p==0):
                 prime = False
                 break
         if(prime):
             primes.append(i)
+    return primes
+
+def find_primes(num_primes):
+    primes = []
+    i = 2
+    while(len(primes)<num_primes):
+        inner_upper = m.sqrt(i)
+        prime = True
+        for p in primes:
+            if (p>inner_upper):
+                break
+            if (i%p==0):
+                prime = False
+                break
+        if(prime):
+            primes.append(i)
+        i+=1
     return primes
